d2718b8357ab2148c98f6843c72d2dc16a7254d7
suhailvs <suhailvs@gmail.com>
2022-01-17 04:43:19 +0530

    get location from ip done

--- django_school/classroom/signals.py
+++ django_school/classroom/signals.py
@@ -6,9 +6,19 @@ from .models import AuditEntry
 
 # https://stackoverflow.com/a/37620866/2351696
 @receiver(user_logged_in)
-def user_logged_in_callback(sender, request, user, **kwargs):  
+def user_logged_in_callback(sender, request, user, **kwargs):
     ip = request.META.get('REMOTE_ADDR')
-    AuditEntry.objects.create(action='user_logged_in', ip=ip, username=user.username)
+
+    from urllib.request import urlopen
+    from json import load
+    try:
+        # https://stackoverflow.com/a/55432323/2351696
+        data = load(urlopen(f'https://ipinfo.io/{ip}/json'))
+        ip_loc = f"{ip}, {data['city']} {data['region']} {data['country']}"
+    except:
+        ip_loc = ip
+
+    AuditEntry.objects.create(action='user_logged_in', ip=ip_loc, username=user.username)
 
 
 @receiver(user_login_failed)
